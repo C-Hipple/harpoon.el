44ebccb1cdfca578e092f0c4640158b50aff73b9
chris <chris.hipple@gmail.com>
2026-02-15 15:22:33 -0500

    bump

--- harpoon.el
+++ harpoon.el
@@ -2,7 +2,7 @@
 
 ;; Author: Chris Hipple
 ;; URL: https://github.com/C-Hipple/harpoon.el
-;; Version: 1.0.4
+;; Version: 1.0.5
 ;; Package-Requires: ((emacs "25.1"))
 
 ;; SPDX-License-Identifier: GPL-3.0+
